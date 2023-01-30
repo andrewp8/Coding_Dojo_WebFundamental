9a1b4897e27a4df194be219fba057e4faa3c95e1
Andrew Pham <minhanh.vietpham@gmail.com>
2023-01-30 17:26:09 -0600

    PC - finished button clicker

---
+++ button_clicker/script.js
@@ -0,0 +1,11 @@
+function switchMode(element) {
+  element.innerText = "Logout";
+}
+
+function custom_alert() {
+  alert("Ninja was liked");
+}
+
+function removeBtn(element) {
+  element.remove();
+}
