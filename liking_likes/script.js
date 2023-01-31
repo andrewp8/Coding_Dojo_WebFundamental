dc53dbab8da09728fca5748af18dfaafa08e7ca4
Andrew Pham <minhanh.vietpham@gmail.com>
2023-01-31 13:14:00 -0600

    PC: Belt_Exam start-up code

--- liking_likes/script.js
+++ liking_likes/script.js
@@ -1,11 +1,28 @@
 function like(element) {
-  console.log(element);
-  var like1 = document.querySelector('#user1');
-  like1.innerText++;
+  var likes = document.querySelectorAll('.likeCount');
+  console.log(likes);
 
-  var like2 = document.querySelector('#user2');
-  like2.innerText++;
+  for (var i = 0; i < likes.length; i++) {
+    if (likes[0].id == 'user1') {
+      var like1 = document.querySelector('#user1');
+      like1.innerText++;
+    }
+    if (likes[0].id == 'user2') {
+      var like2 = document.querySelector('#user2');
+      like2.innerText++;
+    }
+    if (likes[0].id == 'user3') {
+      var like3 = document.querySelector('#user3');
+      like3.innerText++;
+    }
+  }
+  // console.log(element);
+  // var like1 = document.querySelector('#user1');
+  // like1.innerText++;
 
-  var like3 = document.querySelector('#user3');
-  like3.innerText++;
+  // var like2 = document.querySelector('#user2');
+  // like2.innerText++;
+
+  // var like3 = document.querySelector('#user3');
+  // like3.innerText++;
 }
