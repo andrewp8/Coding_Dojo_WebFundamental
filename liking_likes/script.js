23654232e5e15d9d84947af9abfb4c76089e521a
Andrew Pham <minhanh.vietpham@gmail.com>
2023-01-31 14:49:02 -0600

    PC: Finished w. Spencer's help. There is a question in the HTML, but will ask after the exam

--- liking_likes/script.js
+++ liking_likes/script.js
@@ -1,28 +1,5 @@
-function like(element) {
-  var likes = document.querySelectorAll('.likeCount');
-  console.log(likes);
-
-  for (var i = 0; i < likes.length; i++) {
-    if (likes[0].id == 'user1') {
-      var like1 = document.querySelector('#user1');
-      like1.innerText++;
-    }
-    if (likes[0].id == 'user2') {
-      var like2 = document.querySelector('#user2');
-      like2.innerText++;
-    }
-    if (likes[0].id == 'user3') {
-      var like3 = document.querySelector('#user3');
-      like3.innerText++;
-    }
-  }
-  // console.log(element);
-  // var like1 = document.querySelector('#user1');
-  // like1.innerText++;
-
-  // var like2 = document.querySelector('#user2');
-  // like2.innerText++;
-
-  // var like3 = document.querySelector('#user3');
-  // like3.innerText++;
+function like(userId) {
+  var likes = document.querySelector('#' + userId);
+  likes.innerText++;
+  // console.log(likes);
 }
